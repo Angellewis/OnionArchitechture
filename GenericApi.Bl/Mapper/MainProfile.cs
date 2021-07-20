e76dd0a19897734faeb9283f120c710de9c0cd1d
Emmanuel Jimenez <emmanuel.jimenez@solvex.com.do>
2021-07-20 13:02:21 -0400

    Autommaper, Dtos y services pattern

---
+++ GenericApi.Bl/Mapper/MainProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using GenericApi.Bl.Dto;
+using GenericApi.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericApi.Bl.Mapper
+{
+    public class MainProfile : Profile
+    {
+        public MainProfile()
+        {
+            CreateMap<Document, DocumentDto>().ReverseMap();
+
+            CreateMap<Member, MemberDto>()
+                .ForMember(dto => dto.PhotoFileName, config => config.MapFrom(entity => entity.Photo.FileName));
+            
+            CreateMap<MemberDto, Member>();
+        }
+    }
+}
