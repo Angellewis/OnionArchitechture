f0cd364a4126b50ad99a15d19168cfb6277a8d0b
Emmanuel Jimenez <emmanuel.jimenez@solvex.com.do>
2021-07-23 11:17:47 -0400

    serilog initial

---
+++ GenericApi.Bl/Config/SerilogConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GenericApi.Bl.Config
+{
+    public static class SerilogConfig
+    {
+        public static IServiceCollection ConfigSerilog(this IServiceCollection service)
+        {
+        }
+    }
+}
