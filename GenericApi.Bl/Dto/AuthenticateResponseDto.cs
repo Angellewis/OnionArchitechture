1ce77252c69a8cb0aaac9c123cc0f814192c432c
Emmanuel Jimenez <emmanuel.jimenez@solvex.com.do>
2021-07-23 15:31:56 -0400

    Authentication

---
+++ GenericApi.Bl/Dto/AuthenticateResponseDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericApi.Bl.Dto
+{
+    public class AuthenticateResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Token { get; set; }
+
+        public AuthenticateResponseDto(UserDto user, string token)
+        {
+            Id = user.Id.GetValueOrDefault();
+            Name = user.Name;
+            LastName = user.LastName;
+            UserName = user.UserName;
+            Token = token;
+        }
+    }
+}
