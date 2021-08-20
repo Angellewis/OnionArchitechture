6197b41d9f093e71817f325c022ec720a51bb228
Angel Lewis Del Rosario Terrero <20163553@itla.edu.do>
2021-08-20 22:48:43 +0800

    Last Changes

---
+++ GenericApi.Model/Migrations/20210819182741_LastMigration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GenericApi.Model.Migrations
+{
+    public partial class LastMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "StartHour",
+                table: "WorkShopDays",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "EndHour",
+                table: "WorkShopDays",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Dob" },
+                values: new object[] { new DateTimeOffset(new DateTime(2021, 8, 19, 18, 27, 41, 349, DateTimeKind.Unspecified).AddTicks(4501), new TimeSpan(0, 0, 0, 0, 0)), new DateTime(2021, 8, 20, 2, 27, 41, 349, DateTimeKind.Local).AddTicks(4941) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "StartHour",
+                table: "WorkShopDays",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "EndHour",
+                table: "WorkShopDays",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Dob" },
+                values: new object[] { new DateTimeOffset(new DateTime(2021, 8, 6, 16, 50, 35, 53, DateTimeKind.Unspecified).AddTicks(8384), new TimeSpan(0, 0, 0, 0, 0)), new DateTime(2021, 8, 6, 12, 50, 35, 53, DateTimeKind.Local).AddTicks(8877) });
+        }
+    }
+}
